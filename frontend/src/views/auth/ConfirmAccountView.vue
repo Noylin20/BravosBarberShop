c67254c5193fa44a0d0715a96e0ec02643e2b1bc
Noylin20 <noylin.elizondo@ucrso.info>
2023-06-24 22:45:47 -0600

    Cuenta
    Confirmación de cuenta

--- frontend/src/views/auth/ConfirmAccountView.vue
+++ frontend/src/views/auth/ConfirmAccountView.vue
@@ -1,11 +1,37 @@
 
 <script setup>
+import { onMounted, inject } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import AuthAPI from '../../api/AuthAPI'
+
+const toast = inject('toast')
+const router = useRouter()
+const route = useRoute()
+const { token } = route.params
+
+onMounted(async () => {
+  try {
+    const { data } = await AuthAPI.verifyAccount(token)
+    toast.open({
+      message: data.msg,
+      type: 'success'
+    })
+    setTimeout(() => {
+      router.push({ name: 'login' })
+    }, 5000);
+  } catch (error) {
+  toast.open({
+    message: error.response.data.msg,
+    type: 'error'
+  })
+}
+})
+
 </script>
 
 
 <template>
-    <div class="flex justify-between">
-  <h1>Desde Confirm Account</h1>
-  <RouterView/>
-    </div>
+  <h1 class="text-4xl font-extrabold text-black text-center mt-10">Verificar cuenta</h1>
+
+  <RouterView />
 </template>
